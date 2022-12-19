895101948e3fd0068b73b78d77d521e20c35de7f
Alexey Gaidykov <swiftgaida@list.ru>
2022-12-19 20:02:03 +0300

    Initial Commit

---
+++ Movie Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Search
+//
+//  Created by Alexey Gaidykov on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
